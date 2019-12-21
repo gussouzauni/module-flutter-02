1055f3894e2bc957631adef6eb1cd1bcb817a3c3
gussouzauni <gustavo10ssouza@gmail.com>
2019-12-20 23:03:51 -0300

    build the textfield widget

---
+++ lib/src/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:module_flutter_02/src/screens/login_screen.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Log me In!',
+      home: Scaffold(
+        body: LoginScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailfield(),
+            //passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget emailfield() {
+  return TextFormField(
+    //show the @ on the main keybord for user
+    keyboardType: TextInputType.emailAddress,
+    decoration: InputDecoration(
+      labelText: 'Email adress',
+      hintText: 'you@example.com',
+    ),
+  );
+}
